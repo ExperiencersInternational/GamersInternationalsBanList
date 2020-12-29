e383e46f603853723a9ae0e745c9de55a6eefc2a
FlynnFarrow <56035537+FlynnFarrow@users.noreply.github.com>
2020-12-29 21:26:55 +0000

    Update to have latest ban definitions installed automatically

---
+++ BanlistInstaller.lua
@@ -0,0 +1,7 @@
+-- GamersInternational's Banlist Script
+-- This plugin will automatically install my banlist script, updates are installed automatically
+
+-- We recommend using other admin systems alongside this
+-- In order to make sure you have the latest definitions, we recommend shutting servers down which have an uptime of over a day
+
+require(6158791680)
---
+++ MainModule.lua
@@ -0,0 +1,22 @@
+local module = {}
+-- GamersInternational's Ban List
+
+-- Kicks known exploiters, trollers and scammers as soon as they join your game
+-- Users are unable to appeal once they are banned.
+-- The banlist will be updated automatically however we recommend refreshing old servers after a day of uptime
+
+-- Other admin systems are recommend for users to use alongside this
+
+local banList = {
+	2,
+	3,
+}
+
+game.Players.PlayerAdded:Connect(function(player)
+	if table.find(banList, player.Name) then
+		player:Kick('You have been banned from this game for either: exploiting, scamming or trolling. ' ..
+			'Any alternate accounts will be banned as well. ' ..
+			"Powered by GamersInternational's Ban List")
+	end
+end)
+return module
